74d420ebdcd806021ce6a70a918001a7076b9f88
Haxonek <thomash1996@live.com>
2016-01-01 18:45:22 -0600

    Added the book model and controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to root_path
+  end
+
+  private
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :description, :chapters, :tags, :user_id)
+  end
+end
---
+++ db/migrate/20160101235305_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :description
+      t.text :tags
+      t.integer :user_id
+      t.integer :chapters, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
